838e2ac057c380f77b8ba990578f18c769032b79
Marcel Kloubert <marcel.kloubert@gmx.net>
2017-03-18 02:02:49 +0100

    using TypeScript definition files in MAIL plugin now

--- src/plugins/mail.ts
+++ src/plugins/mail.ts
@@ -27,8 +27,9 @@ import * as deploy_contracts from '../contracts';
 import * as deploy_helpers from '../helpers';
 import * as deploy_objects from '../objects';
 import * as i18 from '../i18';
-const Mailer = require('nodemailer');
+import * as Mailer from 'nodemailer';
 import * as Moment from 'moment';
+import * as SMTP from "nodemailer-smtp-transport";
 import * as vscode from 'vscode';
 
 
@@ -117,7 +118,7 @@ https://github.com/mkloubert/vs-deploy`,
                             ]
                         };
 
-                        let transporter = Mailer.createTransport({
+                        let transporter = Mailer.createTransport(<SMTP.SmtpOptions>{
                             host: host,
                             port: port,
                             auth: auth,
